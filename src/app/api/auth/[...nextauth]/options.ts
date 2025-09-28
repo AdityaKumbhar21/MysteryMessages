0d9af9825cdb1ca251956a0cfa0212d523ddcbc7
Aditya Kumbhar <adityakumbhar915@gmail.com>
2025-09-28 12:35:47 +0530

    built signup and signIn logic

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,77 @@
+import CredentialsProvider from "next-auth/providers/credentials"
+import { NextAuthOptions } from "next-auth";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/userModel";
+import bcrypt from "bcrypt"
+
+
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "credentials",
+            credentials: {
+            username: { label: "Email", type: "text"},
+            password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials: any): Promise<any>{
+                await dbConnect()
+
+                try {
+                    const user = await UserModel.findOne({
+                        $or:[
+                            {email: credentials.identifier},
+                            {username: credentials.identifier},
+                        ]
+                    })
+
+                    if(!user){
+                        throw new Error("User does not exist")
+                    }
+
+                    if(!user.isVerified){
+                        throw new Error("Verify your email before login")
+                    }
+
+                    const checkPassword = await bcrypt.compare(credentials.password, user.password)
+
+                    if(!checkPassword){
+                        throw new Error("Incorrect Password")
+                    }
+                    return user
+                } catch (err: any) {
+                    throw new Error(err)
+                }
+            }
+        })
+    ],
+    callbacks:{
+        async session({ session, user, token }) {
+            if(token){
+                session.user._id = token._id;
+                session.user.isVerified = token.isVerified;
+                session.user.isAcceptingMessages = token.isAcceptingMessages;
+                session.user.username = token.username
+            }
+            return session
+        },
+        async jwt({ token, user }) {
+            if(user){
+                token._id = user._id;
+                token.isVerified = user.isVerified;
+                token.isAcceptingMessages = user.isAcceptingMessages;
+                token.username = user.username
+            }
+            return token
+        }
+    },
+    pages:{
+        signIn: "/sign-in"
+    },
+    session:{
+        strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+
+}
--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -11,6 +11,87 @@ export async function POST(request: Request) {
 
         const {username, email, password} = await request.json()
 
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified: true
+        })
+
+        if(existingUserVerifiedByUsername){
+            return Response.json({
+            success: false,
+            messages: "Username is already taken"
+            },{
+                status:401
+            })
+        }
+
+        const verifyCode = Math.floor(100000 + Math.random()*900000).toString()
+        const existingUserByEmail = await UserModel.findOne({email})
+
+        if(existingUserByEmail){
+
+            if(existingUserByEmail.isVerified){
+                return Response.json({
+                success: false,
+                messages: "Email is already exists"
+                },{
+                    status:401
+                })
+            }
+
+            else{
+                const hashedPassword = await bcrypt.hash(password, 10)
+                existingUserByEmail.password = hashedPassword
+                existingUserByEmail.verifyCode = verifyCode
+                const expiryDate = new Date()
+                expiryDate.setHours(expiryDate.getHours() + 1)
+                existingUserByEmail.verifyCodeExpiry = expiryDate
+                await existingUserByEmail.save()
+                
+            }
+        }
+
+        else{
+            const hashedPassword = await bcrypt.hash(password, 10)
+            const expiryDate = new Date()
+            expiryDate.setHours(expiryDate.getHours() + 1)
+            const newUser = new UserModel({
+                username,
+                email,
+                password: hashedPassword,
+                verifyCode,
+                isVerified: false,
+                verifyCodeExpiry: expiryDate,
+                isAcceptingMessages: true,
+                messages: []
+            })
+
+            await newUser.save()
+
+        }
+
+
+        //send verification email
+        const verificationEmail = await sendVerficationEmail(email, username, verifyCode)
+
+        if(!verificationEmail.success){
+            return Response.json({
+            success: false,
+            messages: verificationEmail.message
+            },{
+                status:500
+            })
+        }
+
+        return Response.json({
+            success: true,
+            messages: "User registered succesfully. Verify your email"
+            },{
+                status:201
+            })
+        
+
+
     } catch (error) {
         console.error("Error in signup route: ", error);
         return Response.json({
